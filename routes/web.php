7cae1c8603da94df0c16f33fad20570f855a853f
mayulondhe16 <mayulondhe16@gmail.com>
2023-07-05 10:41:38 +0530

    Reverted middleware changes

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,8 @@ Route::group([
     'prefix' => 'api'
 
 ],function ($router) {
-    Route::post('login', 'AuthController@login')->middleware('\App\Http\Middleware\CorsMiddleware::class');;
+    // Route::post('login', 'AuthController@login')->middleware('\App\Http\Middleware\CorsMiddleware::class');
+    Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('user-profile', 'AuthController@me');
