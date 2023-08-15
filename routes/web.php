ae0776ee2b51a8e6f1b88673fdd5291e6df4e4a0
Satish <satish@rmail.com>
2023-08-15 18:14:13 +0530

    changes for project details api name changes

--- routes/web.php
+++ routes/web.php
@@ -172,8 +172,8 @@ $router->group(['middleware' => 'auth','prefix' => 'api'], function ($router)
 
     Route::get('get_handson_project_details', 'HandonProjectControllerController@getProjectDetails');
     Route::post('add_handson_project_details', 'HandonProjectControllerController@addProjectDetails');
-    Route::post('update_handson_project_details/{id}', 'HandonProjectControllerController@updateDetails');
-    Route::delete('delete_handson_project_details/{id}', 'HandonProjectControllerController@deleteDetails');
+    Route::post('update_handson_project_details/{id}', 'HandonProjectControllerController@updateProjectDetails');
+    Route::delete('delete_handson_project_details/{id}', 'HandonProjectControllerController@deleteProjectDetails');
 
     Route::get('get_course_fee_details_list', 'CourseFeeDetailsController@getCourseFeeDetailsList');
     Route::post('add_course_fee_details', 'CourseFeeDetailsController@add');
