9413e48b44dccca3965b60ec7cf8badaa3b30b86
mayulondhe16 <mayulondhe16@gmail.com>
2023-08-17 17:49:27 +0530

    Added handson get issue

--- app/Http/Controllers/HandonProjectControllerController.php
+++ app/Http/Controllers/HandonProjectControllerController.php
@@ -51,7 +51,7 @@ class HandonProjectControllerController extends Controller
             $response[] = $data;
         }
 
-        return response()->json(['status' => 'Success', 'message' => 'Data get successfully','StatusCode'=>'200']);
+        return response()->json(['status' => 'Success', 'message' => 'Data get successfully','StatusCode'=>'200','data'=>$data]);
     }
 
     public function deleteCategory($id)
