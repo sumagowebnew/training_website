8726edf95ef1259e78cce185aac81e0da3062d0b
mayulondhe16 <mayulondhe16@gmail.com>
2023-08-18 12:14:36 +0530

    Added data

--- app/Http/Controllers/AluminiController.php
+++ app/Http/Controllers/AluminiController.php
@@ -41,10 +41,30 @@ class AluminiController extends Controller
 
     public function index(Request $request, $id)
     {
-        $all_data = Alumini::where('course_id',$request->id)->get()->toArray();
-        return response()->json(['data'=>$all_data,'status' => 'Success', 'message' => 'Fetched All Data Successfully','StatusCode'=>'200']);
-    }
+        $all_data = Alumini::where('course_id',$request->id)->get();
+        $response = [];
 
+        foreach ($all_data as $item) {
+
+            $logo = $item['image'];
+
+            $imagePath =str_replace('\\', '/', base_path())."/storage/all_web_data/images/alumini/" . $logo;
+
+            $base64 = "data:image/png;base64," . base64_encode(file_get_contents($imagePath));
+
+            $data['image'] = $base64; 
+            $data['designation'] = $item['designation'];
+            $data['company'] = $item['company'];
+            $data['name'] = $item['name'];
+            $data['id'] = $item['id'];
+
+            $response[] = $data;
+        }
+
+
+        return response()->json(['data'=>$response,'status' => 'Success', 'message' => 'Fetched All Data Successfully','StatusCode'=>'200']);   
+    }
+    
     public function all_alumini(Request $request)
     {
         $all_data = Alumini::get()->toArray();
