8dc1657008e5dda126ace0c0650f543e7fbd2a4c
mayulondhe16 <mayulondhe16@gmail.com>
2023-09-07 15:11:53 +0530

    CORS issue

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
@@ -16,12 +15,17 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Origin'      => 'https://trainingadmin.sumagotest.in/',
-            'Access-Control-Allow-Methods'     => 'POST, GET, PUT, DELETE',
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
+
+        if ($request->isMethod('OPTIONS'))
+        {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
 
         $response = $next($request);
         foreach($headers as $key => $value)
@@ -32,4 +36,3 @@ class CorsMiddleware
         return $response;
     }
 }
-?>
