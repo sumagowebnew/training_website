9b61187d9fb80cc9fe91f38a399cfd21f79730e1
mayulondhe16 <mayulondhe16@gmail.com>
2023-09-07 14:46:15 +0530

    CORS issue

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
@@ -14,25 +15,11 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
-        ];
-
-        if ($request->isMethod('OPTIONS'))
-        {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
-
         $response = $next($request);
-        foreach($headers as $key => $value)
-        {
-            $response->header($key, $value);
-        }
+
+        $response->header('Access-Control-Allow-Origin','*');
 
         return $response;
     }
 }
+?>
