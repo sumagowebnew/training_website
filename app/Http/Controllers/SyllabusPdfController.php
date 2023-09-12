b3f3c60f6ffb30f3cdbbed3c6941d877b778cbd0
Satish <satishaurange@sumagoinfotech.com>
2023-09-12 14:43:35 +0530

    changes for the pdf api

--- app/Http/Controllers/SyllabusPdfController.php
+++ app/Http/Controllers/SyllabusPdfController.php
@@ -23,6 +23,28 @@ class SyllabusPdfController extends Controller
         return response()->json($response);
     }
 
+
+    public function getAllDataList(Request $request)
+    {
+        $certificate = SyllabusPdf::leftJoin('subcourses', function($join) {
+            $join->on('syllabuspdf.sub_course_id', '=', 'subcourses.id');
+          })
+        //  ->where('syllabuspdf.subcourse_id',$request->id)
+         ->select('syllabuspdf.*',
+         'subcourses.name'
+         )
+         ->get();
+        $response = [];
+        foreach ($certificate as $item) {
+            $data = $item->toArray();
+            $logo = $data['file'];
+            $file = "https://trainingapi.sumagotest.in/storage/all_web_data/images/syllabus_pdf/".$data['file'];
+            $data['file'] = $file;
+            $response[] = $data;
+        }
+        return response()->json($response);
+    }
+
     public function Add(Request $request)
     {
         $validator = Validator::make($request->all(), [
