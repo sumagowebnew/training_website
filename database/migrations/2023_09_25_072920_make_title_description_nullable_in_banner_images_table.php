7715d92ce531e6cfab84f8b15dcbb6a399251676
mayulondhe16 <mayulondhe16@gmail.com>
2023-09-25 13:03:50 +0530

    Did title description field nullable in banner images table

---
+++ database/migrations/2023_09_25_072920_make_title_description_nullable_in_banner_images_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('banner_images', function (Blueprint $table) {
+            $table->string('title')->nullable()->change();
+            $table->string('description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('banner_images', function (Blueprint $table) {
+            //
+        });
+    }
+};
