88fba968b5dd322ece53a6fa3e83373042b7a19b
mayulondhe16 <mayulondhe16@gmail.com>
2023-10-19 16:51:34 +0530

    added coursename in faq  api

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -11,14 +11,34 @@ class FaqController extends Controller
 {
     public function index(Request $request)
     {
-        $all_data = Faq::where('course_id',$request->id)->get()->toArray();
-        return response()->json(['data'=>$all_data,'status' => 'Success', 'message' => 'Fetched All Data Successfully','StatusCode'=>'200']);
+        // $all_data = Faq::where('course_id',$request->id)->get()->toArray();
+        $all_data = Faq::join('subcourses', 'subcourses.id', '=', 'faq.course_id')
+        ->where('course_id',$request->id)
+        ->get(['faq.*','subcourses.name AS subcoursename']);
+
+        $response = [];
+        foreach ($all_data as $item) {
+            $data = $item->toArray();
+            $data['table_name'] = 'faq';
+            $response[] = $data;
+            
+        }
+        return response()->json(['data'=>$response,'status' => 'Success', 'message' => 'Fetched All Data Successfully','StatusCode'=>'200']);
     }
 
     public function all_faq(Request $request)
     {
-        $all_data = Faq::get()->toArray();
-        return response()->json(['data'=>$all_data,'status' => 'Success', 'message' => 'Fetched All Data Successfully','StatusCode'=>'200']);
+        $all_data = Faq::join('subcourses', 'subcourses.id', '=', 'faq.course_id')
+        ->get(['faq.*','subcourses.name AS subcoursename']);
+
+        $response = [];
+        foreach ($all_data as $item) {
+            $data = $item->toArray();
+            $data['table_name'] = 'faq';
+            $response[] = $data;
+            
+        }
+        return response()->json(['data'=>$response,'status' => 'Success', 'message' => 'Fetched All Data Successfully','StatusCode'=>'200']);        return response()->json(['data'=>$all_data,'status' => 'Success', 'message' => 'Fetched All Data Successfully','StatusCode'=>'200']);
     }
     public function Add(Request $request)
     {
