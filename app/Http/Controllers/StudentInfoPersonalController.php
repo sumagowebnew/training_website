9963b8fbfdddbdc1404f3e38e5c09133e4aad0f4
mhaskemahesh <maheshmhaske500@gmail.com>
2025-03-18 10:40:50 +0530

    joning date added

--- app/Http/Controllers/StudentInfoPersonalController.php
+++ app/Http/Controllers/StudentInfoPersonalController.php
@@ -179,8 +179,8 @@ class StudentInfoPersonalController extends Controller
     {
 
         $student_info = StudentPersonalInfo::where('student_personal_info.id',$id)
-        ->leftJoin('student_info', 'student_personal_info.stude_id', '=', 'student_info.id')
-        ->select('student_personal_info.id','student_personal_info.fname','student_personal_info.mname','student_personal_info.fathername',
+        ->leftJoin('student_info', 'student_personal_info.id', '=', 'student_info.stude_id')
+        ->select('student_personal_info.id','student_info.stude_id','student_personal_info.fname','student_personal_info.mname','student_personal_info.fathername',
         'student_personal_info.lname','student_personal_info.gender','student_personal_info.parmanenat_address','student_personal_info.current_address',
         'student_personal_info.contact_details','student_personal_info.email','student_personal_info.dob',
         'student_personal_info.whatsappno', 'student_personal_info.age', 'student_personal_info.blood', 'student_personal_info.aadhar',
