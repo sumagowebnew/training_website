ebf423f099f1fb249ca29aede5aca6221652099c
Satish <satishaurange@gmail.com>
2025-07-29 10:45:06 +0530

    changes for the get certificate

--- app/Http/Controllers/TraningCertificateController.php
+++ app/Http/Controllers/TraningCertificateController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use  App\Models\TraningCertificate;
 use Validator;
+use DB;
 
 class TraningCertificateController extends Controller
 {
